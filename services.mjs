5ef0982e64fea5a0fa766c41c64f31ac136d7869
Anders Houbak Kristiansen <palantus@gmail.com>
2022-01-20 11:54:22 +0100

    Permissions

--- services.mjs
+++ services.mjs
@@ -3,11 +3,8 @@ import Role from "../../models/role.mjs"
 
 export default async () => {
   
-  Permission.lookupOrCreate("wiki.edit")
-  Permission.lookupOrCreate("wiki.read")
-
   // init
-  Role.lookupOrCreate("wiki").addPermission(["wiki.edit", "wiki.read"])
+  Role.lookupOrCreate("wiki").addPermission(["wiki.edit", "wiki.read"], true)
 
   return {
   }
