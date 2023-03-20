cab10c131c5f1fecbbd6887136d5288e5448e358
Anders Houbak Kristiansen <palantus@gmail.com>
2023-03-20 13:14:09 +0100

    Menu change

--- ui.mjs
+++ ui.mjs
@@ -1,11 +1,17 @@
-export let menu = [
+export let menu = [,
+  {
+    title: "Me",
+    public: true,
+    items: [
+      {title: "Documents", path: "/wiki-docs", permission: "wiki.edit"},
+      {title: "Personal Wiki", path: "/wiki/index-private", permission: "wiki.edit"}
+    ]
+  },
   {
     title: "Wiki",
     public: true,
     items: [
-      {title: "Documents", path: "/wiki-docs", permission: "wiki.edit"},
       {title: "Index", path: "/wiki", public: true, hideWhenSignedIn: true},
-      {title: "Personal", path: "/wiki/index-private", permission: "wiki.edit"},
       {title: "Public", path: "/wiki/index-public", permission: "wiki.read"},
       {title: "Setup", path: "/wiki/setup", permission: "wiki.setup"},
       {title: "Shared", path: "/wiki/index-shared", permission: "wiki.read"}
