1c00f3ec072f90acffcbf824b139211402d8038b
Anders Kristiansen <palantus@gmail.com>
2024-03-26 10:14:01 +0100

    Add isOfType

--- models/page.mjs
+++ models/page.mjs
@@ -1,4 +1,4 @@
-import Entity, {duplicate, sanitize, query} from "entitystorage"
+import Entity, { duplicate, sanitize, query } from "entitystorage"
 import { getTimestamp } from "../../../tools/date.mjs"
 import Showdown from "showdown"
 import ACL from "../../../models/acl.mjs"
@@ -7,10 +7,10 @@ import Share from "../../../models/share.mjs";
 import User from "../../../models/user.mjs";
 
 class Page extends Entity {
-  
-  initNew(id, user){
+
+  initNew(id, user) {
     id = Page.createId(id)
-    if(Page.lookup(id))
+    if (Page.lookup(id))
       throw "Wiki page already exists"
 
     this.id = id;
@@ -21,9 +21,9 @@ class Page extends Entity {
     ACL.setDefaultACLOnEntity(this, user, DataType.lookup("wiki"))
   }
 
-  static createPrivateIndex(id, user){
+  static createPrivateIndex(id, user) {
     let page = Page.lookup(id)
-    if(!page){
+    if (!page) {
       page = new Page(id, user)
       page.rel(user, "owner")
       page.body = `Hi ${user.name},\n\nWelcome to your private wiki page!`
@@ -35,18 +35,23 @@ class Page extends Entity {
   }
 
   static lookup(id, revisionId) {
-    if(!id) return null;
+    if (!id) return null;
     return revisionId ? query.type(Page).id(revisionId).tag("wiki").tag("revision").prop("id", id).first
-                      : query.type(Page).prop("id", id).tag("wiki").not(query.tag("revision")).first
+      : query.type(Page).prop("id", id).tag("wiki").not(query.tag("revision")).first
   }
 
-  static createId(id){
+  static createId(id) {
     return id.replace(/^\s+|\s+$/g, '') // trim
-             .toLowerCase()
-             .replace(/\//g, '-') //Replace / with -
-             .replace(/[^a-z0-9 -]/g, '') // remove invalid chars
-             .replace(/\s+/g, '-') // collapse whitespace and replace by -
-             .replace(/-+/g, '-'); // collapse dashes
+      .toLowerCase()
+      .replace(/\//g, '-') //Replace / with -
+      .replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+      .replace(/\s+/g, '-') // collapse whitespace and replace by -
+      .replace(/-+/g, '-'); // collapse dashes
+  }
+
+  static isOfType(entity) {
+    if (!entity) return false;
+    return entity.tags?.includes("wiki") || false;
   }
 
   convertBody() {
@@ -60,54 +65,54 @@ class Page extends Entity {
       requireSpaceBeforeHeadingText: true
     })
     this.html = converter.makeHtml(bodyConverted)
-                         .replace(/(\/img\/([\da-zA-Z]+))/g, (src, uu, id) => {
-                            //Replace image urls
-                            let image = this.rels.image?.find(i => i.hash == id) || null
-                            return `${global.sitecore.apiURL}/wiki/image/${id}?shareKey=${image?.shareKey || ""}`
-                          });
+      .replace(/(\/img\/([\da-zA-Z]+))/g, (src, uu, id) => {
+        //Replace image urls
+        let image = this.rels.image?.find(i => i.hash == id) || null
+        return `${global.sitecore.apiURL}/wiki/image/${id}?shareKey=${image?.shareKey || ""}`
+      });
   }
 
-  static idToTitle(id){
+  static idToTitle(id) {
     return id.charAt(0).toUpperCase() + id.slice(1).replace(/\-/g, " ")
   }
 
-  hasAccess(user, right = 'r', shareKey){
+  hasAccess(user, right = 'r', shareKey) {
     return new ACL(this.getPageForAccessValidation(), DataType.lookup("wiki")).hasAccess(user, right, shareKey)
   }
 
-  validateAccess(res, right, respondIfFalse = true){
+  validateAccess(res, right, respondIfFalse = true) {
     return new ACL(this.getPageForAccessValidation(), DataType.lookup("wiki")).validateAccess(res, right, respondIfFalse)
   }
 
-  static validateAccessImage(res, hash, respondIfFalse = true){
+  static validateAccessImage(res, hash, respondIfFalse = true) {
     let idSet = new Set()
-    for(let page of Page.search(`tag:wiki image.prop:"hash=${hash}"`).map(p => p.getPageForAccessValidation())){
-      if(idSet.has(page._id)) continue;
+    for (let page of Page.search(`tag:wiki image.prop:"hash=${hash}"`).map(p => p.getPageForAccessValidation())) {
+      if (idSet.has(page._id)) continue;
       idSet.add(page._id)
-      if(page.validateAccess(res, 'r', respondIfFalse))
+      if (page.validateAccess(res, 'r', respondIfFalse))
         return true;
     }
     return false;
   }
 
-  rights(user){
-    if(user.permissions.includes("admin")) return 'rw'
+  rights(user) {
+    if (user.permissions.includes("admin")) return 'rw'
     let acl = new ACL(this.getPageForAccessValidation(), DataType.lookup("wiki"))
-    return "" + (acl.hasAccess(user, "r")?'r':'') + (acl.hasAccess(user, "w")?'w':'')
+    return "" + (acl.hasAccess(user, "r") ? 'r' : '') + (acl.hasAccess(user, "w") ? 'w' : '')
   }
 
-  getPageForAccessValidation(){
+  getPageForAccessValidation() {
     return this.isRevision ? Page.from(this.relsrev.revision?.[0]) : this;
   }
 
-  get isRevision(){
+  get isRevision() {
     return this.tags.includes("revision")
   }
 
-  delete(){
+  delete() {
     this.rels.image?.forEach(i => {
       this.removeRel(i, "image")
-      if(!i.relsrev.image){
+      if (!i.relsrev.image) {
         i.delete();
       }
     })
@@ -116,64 +121,64 @@ class Page extends Entity {
     super.delete();
   }
 
-  storeRevision(author){
-    if(this.body === undefined || this.isRevision) return;
+  storeRevision(author) {
+    if (this.body === undefined || this.isRevision) return;
     let revision = duplicate(this).tag("revision")
     this.rel(revision, "revision")
     this.rel(author, "author", true)
-    for(let [rev, e] of Object.entries(revision.rels.revision || {})) 
+    for (let [rev, e] of Object.entries(revision.rels.revision || {}))
       revision.removeRel(e, rev); // Don't keep revisions of revisions
   }
 
-  static all(){
+  static all() {
     return query.type(Page).tag("wiki").not(query.tag("revision")).all
   }
 
-  get userTags(){
+  get userTags() {
     return this.tags.filter(t => t.startsWith("user-")).map(t => t.substring(5))
   }
 
-  get revisions(){
+  get revisions() {
     return this.rels.revision?.map(r => Page.from(r)) || []
   }
 
-  get author(){
+  get author() {
     return User.from(this.related.author)
   }
 
-  toObj(user){
+  toObj(user) {
     let title = this.title || (this.id == "index" ? "Wiki Index" : Page.idToTitle(this.id))
     if (this.body && !this.html)
       this.convertBody()
 
     let isRevision = this.isRevision
     let author = this.related.author
-    return{
-      id: this.id, 
+    return {
+      id: this.id,
       revisionId: isRevision ? this._id : null,
-      title, 
-      body: this.body, 
-      html : this.html || "", 
-      exists: true, 
+      title,
+      body: this.body,
+      html: this.html || "",
+      exists: true,
       tags: this.userTags,
       rights: this.rights(user),
       modified: this.modified,
       revisions: isRevision ? [] : (this.rels.revision?.map(r => {
         let author = r.related.author
         return {
-          id: r._id, 
-          modified: r.modified, 
-          author: author ? {id: author.id, name: author.name} : null
+          id: r._id,
+          modified: r.modified,
+          author: author ? { id: author.id, name: author.name } : null
         }
       })) || [],
-      author: author ? {id: author.id, name: author.name} : null
+      author: author ? { id: author.id, name: author.name } : null
     }
   }
 
-  static nullObj(id, res){
-    if(res?.locals.user && res?.locals.user.id != "guest" && id == `index-private-${res?.locals.user.id}`)
+  static nullObj(id, res) {
+    if (res?.locals.user && res?.locals.user.id != "guest" && id == `index-private-${res?.locals.user.id}`)
       return Page.createPrivateIndex(id, res.locals.user).toObj(res.locals.user)
-    return { id, title: (id == "index" ? "Wiki Index" : Page.idToTitle(id)), body: "", html: "", exists: false, tags: [], rights: "rw", revisions: [], revisionId: null}
+    return { id, title: (id == "index" ? "Wiki Index" : Page.idToTitle(id)), body: "", html: "", exists: false, tags: [], rights: "rw", revisions: [], revisionId: null }
   }
 }
 
